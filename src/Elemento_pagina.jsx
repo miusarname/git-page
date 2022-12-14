4ecf22977dfcffc26c214b49f5278c8a804fa75e
miusarname <oscarm.alvarezg@gmail.com>
2022-12-14 11:14:04 -0500

    Se mejoraron los elementos y se crearon los estilos
    un gran commit

--- src/Elemento_pagina.jsx
+++ src/Elemento_pagina.jsx
@@ -1,10 +1,26 @@
-export function Navbar(props){
-    return<div className="Contenedor_Barra_de_Navegacion">
-    <nav className="Navbar">
-      <li className={props.clase}><a>{props.title1}</a></li>
-      <li className={props.clase}><a>{props.title2}</a></li> 
-      <li className={props.clase}><a>{props.title3}</a></li>
-    </nav></div>
-};
+import PropTypes from "prop-types";
 
-/* export function */
+/* barra de navegacion de la pagina  web */
+export function Navbar(props) {
+  return (
+    <div className="Contenedor_Barra_de_Navegacion">
+      <nav className="Navbar">
+        <li className={props.clase}>
+          <a>{props.title1}</a>
+        </li>
+        <li className={props.clase}>
+          <a>{props.title2}</a>
+        </li>
+        <li className={props.clase}>
+          <a>{props.title3}</a>
+        </li>
+      </nav>
+    </div>
+  );
+}
+
+Navbar.defaultProps={
+  clase :"esta es una clase"
+}
+
+
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <>
-    <Navbar title1="Inicio" title2="Nuestro projectos" title3="Sobre" clase="items"/>
+    <Navbar title1="Inicio" title2="Nuestro projectos" title3="Sobre" clase="items" />
   </>
 );
