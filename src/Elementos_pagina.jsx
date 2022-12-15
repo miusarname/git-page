cc2b02c7e92cea964303f51561cd02de239851ed
miusarname <oscarm.alvarezg@gmail.com>
2022-12-15 11:42:45 -0500

    lleve a un nuevo nivel la applicaions

---
+++ src/Elementos_pagina.jsx
@@ -0,0 +1,110 @@
+import PropTypes from "prop-types";
+import { Component } from "react";
+
+export function NavBar(props){
+  return<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href={props.linkOfImage}>
+      <img src={props.linkImage} width="112" height="28"/>
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        {props.title1}
+      </a>
+
+      <a class="navbar-item">
+      {props.title2}
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+        {props.barIndex}
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+          {props.subtitle1}
+          </a>
+          <a class="navbar-item">
+          {props.subtitle2}
+          </a>
+          <a class="navbar-item">
+          {props.subtitle3}
+          </a>
+          <hr class="navbar-divider"/>
+          <a class="navbar-item">
+          {props.subtitle4}
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+}
+
+export function Hero(props){
+  return <div>
+    <section class="hero is-success is-fullheight ">
+  <div class="hero-body">
+    <div class="">
+      <p class="title">
+        {props.titleH}
+      </p>
+      <p class="subtitle">
+        {props.subtitleH}
+      </p>
+    </div>
+  </div>
+</section>
+  </div>
+}
+
+export function Present(props){
+  return <div class="notification is-white p-6 m-6">
+  <article class="message is-primary ">
+  <div class="message-header">
+    {props.header}
+  </div>
+  <div class="message-body">
+    {props.text}
+  </div>
+</article>
+</div>
+}
+
+ export function Formulario(props){
+  return <form onSubmit={(e)=>{e.preventDefault();alert("enviado")}}> {/* por arreglar */}
+    <h2>Nombre:</h2>
+    <input type="text"></input>
+    <h2>Edad</h2>
+    <input type="number"></input>
+    <h2>dirección:</h2>
+    <input type="text"></input>
+    <h2>Motivo por el que deseas aprender...</h2>
+    {/* <input type="checkbox">{props.razon1}</input>  */}  {/* por arreglar */}
+  </form>
+
+}
+
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Navbar,Hero,app,ListItem,Formulario } from "./Elemento_pagina";
+import { Formulario,NavBar,Hero,Present } from "./Elementos_pagina";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <>
-  <ListItem linkImage ="https://personalpageomag.netlify.app/media/Head.ico"/>
-  {/* Creo la barra de navegacion pasando lo que contendra los <a> y los <li> */}
-    <Navbar title1="Inicio" title2="Nuestro projectos" title3="Sobre" clase="items" />
-    {/* Se le debe pasar lo que contendra el h1 con (title) y un texto extra con (text) */}
-    <Hero title="Pagina de desarrollador" text="un proyecto de cooperacion entre dos futuros ingenieros"/>
-    
+    <NavBar linkOfImage="#" linkImage="#" title1="Inicio" title2="Sobre Mi" barIndex="Más Información..." subtitle1="Proyectos" subtitle2="Habilidades" subtitle3="Vision" subtitle4="Redes Sociales"/>
+    <Hero titleH="Pagina de un par de programadores" subtitleH="Esta pagina fue creada por un par de programadores que desean obtner experiencia en el trabajo en equipos..."/>
+    <Present header="Que tal? Soy Oscar Alvarez G" text="Soy un programador Autodidacta aunque suele tambien estudiar con academias de programacion como freeCodeCamp además de que deseo ir a la Universidad a estudiar Ingenieria de Sistemas e Informatica" />
   </>
 );
